01dd6f6507970136ec382913a287c04458eaa8e2
Sreevidhya22 <sreevidhyavenugopal22@gmail.com>
2025-09-09 14:44:55 +0530

    Add files via upload

---
+++ navishirt.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
+data = pd.read_csv('dataset.csv')
+X = data[['HEIGHT', 'WEIGHT']]
+y = data['TSHIRT-SIZE']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+model = GaussianNB()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+print("Enter the sample data:")
+h = int(input("Height in cm: "))
+w = int(input("Weight in kg: "))
+sample = pd.DataFrame([[h, w]], columns=['HEIGHT', 'WEIGHT'])
+pred_label = model.predict(sample)
+print("Predicted T-shirt size:", pred_label[0])
+
+
