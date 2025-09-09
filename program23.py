87f463b7b39de5ff121e9ef2f054ee26ad526976
Sreevidhya22 <sreevidhyavenugopal22@gmail.com>
2025-09-09 14:14:27 +0530

    Add files via upload

---
+++ program23.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder
+from sklearn import metrics
+data = pd.read_csv('dataset.csv')  
+X = data[['HEIGHT', 'WEIGHT']]
+y = data['TSHIRT-SIZE']
+le = LabelEncoder()
+y_encoded = le.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.3, random_state=1)
+model = GaussianNB()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+print("Enter the sample data:")
+h = int(input("Height in cm: "))
+w = int(input("Weight in kg: "))
+sample = pd.DataFrame([[h, w]], columns=['HEIGHT', 'WEIGHT'])
+pred = model.predict(sample)
+pred_label = le.inverse_transform(pred)
+print("Predicted T-shirt size:", pred_label[0])
